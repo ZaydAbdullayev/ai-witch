95b288df41e0441cd09efde45a597919f8bc1d1e
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-04-21 20:38:13 +0500

    new

---
+++ src/mocks/ai.js
@@ -0,0 +1,21 @@
+import doctor from '../assets/wizard1.png';
+import scarlet from '../assets/wizard2.png';
+import shali from '../assets/wizard3.png';
+import devergent from '../assets/wizard4.png';
+
+export const heros = [
+    { id: 1, name: "Scarlet Witch", img: scarlet, description: "Scarlet Witch is a character in the game. She is a powerful character with a lot of abilities. She is a great warrior and a great leader. She is a great character to play with." },
+    { id: 2, name: "Doctor Strange", img: doctor, description: "Doctor Strange is a character in the game. He is a powerful character with a lot of abilities. He is a great warrior and a great leader. He is a great character to play with." },
+    { id: 3, name: "Shali", img: shali, description: "Shali is a character in the game. She is a powerful character with a lot of abilities. She is a great warrior and a great leader. She is a great character to play with." },
+    { id: 4, name: "Devergent", img: devergent, description: "Devergent is a character in the game. He is a powerful character with a lot of abilities. He is a great warrior and a great leader. He is a great character to play with." },
+]
+
+
+export const logs = [
+    { text: 'PROJECT DEVELOPMENT UPDATE', date: new Date().toISOString() },
+    { text: 'new project started', date: new Date().toISOString() },
+    { text: 'release story', date: new Date().toISOString() },
+    { text: 'visual updates', date: new Date().toISOString() },
+    { text: 'going public', date: new Date().toISOString() },
+    { text: 'beta program', date: new Date().toISOString() },
+]
